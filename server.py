b7759772b297cb388799400a5e1670960b0c638c
GXHST-0 <147278898+GXHST-0@users.noreply.github.com>
2024-07-28 15:42:57 +0200

    Add SensorData model

--- server.py
+++ server.py
@@ -5,3 +5,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
 db = SQLAlchemy(app)
+class SensorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    motion = db.Column(db.Boolean)
+    sound = db.Column(db.Boolean)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
