670847f136680662efa00ee83a352c44527b6166
GXHST-0 <147278898+GXHST-0@users.noreply.github.com>
2024-07-28 15:24:34 +0200

    Initial Flask server setup with SQLAlchemy

---
+++ server.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
+db = SQLAlchemy(app)
